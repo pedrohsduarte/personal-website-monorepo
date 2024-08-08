15b9b4008625d483c2d5347575c90c2ddad9e50d
pedrohsduarte <pedrohsduarte@gmail.com>
2024-08-08 18:38:48 -0300

    feat(repo): add commitlint

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,54 +1,49 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'turbo',
-    'plugin:prettier/recommended',
-    'plugin:import/recommended',
+    "eslint:recommended",
+    "prettier",
+    "turbo",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "plugin:import/recommended",
   ],
   plugins: [
-    '@typescript-eslint',
-    'prettier',
-    'eslint-plugin-import-helpers',
-    'import',
-    'unused-imports',
+    "@typescript-eslint",
+    "prettier",
+    "eslint-plugin-import-helpers",
+    "import",
+    "unused-imports",
   ],
-  ignorePatterns: [
-    '**/node_modules/**',
-    '**/tests/**',
-    '**/scripts/**/*',
-    '**/coverage/**',
-  ],
+  ignorePatterns: ["**/node_modules/**", "**/coverage/**"],
   overrides: [],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 12,
-    sourceType: 'module',
+    ecmaVersion: 2022,
+    sourceType: "module",
   },
   rules: {
-    '@typescript-eslint/no-use-before-define': ['error'],
-    '@typescript-eslint/no-shadow': ['error'],
-    '@typescript-eslint/explicit-function-return-type': [
-      'error',
+    "@typescript-eslint/no-use-before-define": ["error"],
+    "@typescript-eslint/no-shadow": ["error"],
+    "@typescript-eslint/explicit-function-return-type": [
+      "error",
       { allowExpressions: true },
     ],
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      { js: 'never', ts: 'never', tsx: 'never' },
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      { js: "never", ts: "never", tsx: "never" },
     ],
-    'max-len': ['warn', { code: 100, ignoreComments: true, ignoreUrls: true }],
-    'prettier/prettier': ['error', { endOfLine: 'auto' }],
-    '@typescript-eslint/no-explicit-any': 'warn',
+    "max-len": ["warn", { code: 100, ignoreComments: true, ignoreUrls: true }],
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
+    "@typescript-eslint/no-explicit-any": "warn",
   },
   settings: {
-    'import/resolver': {
+    "import/resolver": {
       node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
       },
       typescript: {},
     },
